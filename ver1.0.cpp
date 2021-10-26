2aadce7b8dc31d9c637c9aae514f3efa9771ca8c
Piotr Bożek <piotrbozek2000@gmail.com>
2021-10-26 15:09:31 +0200

    Wersja 1.0 z wykluczniem 0

--- ver1.0.cpp
+++ ver1.0.cpp
@@ -7,6 +7,6 @@ int main() {
 	float x,a,b;
 	cout<<"podaj b ::  ", cin>>b, cout<<"podaj a ::  ", cin>> a;
 	if(a==0) cout<<"a musi byc a/=0 podaj a ::  ", cin>> a;
-	x=-b/a, cout<<"miejsce zerowe to "<< x ;
+	x=-b/a, cout<<"miejsce zerowe to "<< x ;   
 }
 
